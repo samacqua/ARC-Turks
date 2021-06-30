c37ada289d99cb9086bb057d7a1f70b6d90e6c8e
Sam Acquaviva <samacqua@gmail.com>
2021-06-30 17:55:54 -0400

    Fixed page navigation for test page.

--- js/test.js
+++ js/test.js
@@ -10,10 +10,37 @@ $(window).on('load', function () {
 
 function test_load(task) {
     loadTask(task);
-
     $('#task_name').text('Task: ' + task);
+    updateUrl({"task": task});
+}
 
-    window.history.replaceState(task, task, `/test.html?task=${task}`);
+/**
+ * Handle back and forth navigation
+ * from https://stackoverflow.com/a/3354511/5416200 (but url stored in query params)
+ * @param {Object} e the event that carries the 
+ */
+window.onpopstate = function(e){
+    console.log(e);
+    if(e.state){
+        loadTask(e.state.task);
+        $('#task_name').text('Task: ' + e.state.task);
+        document.title = e.state.pageTitle;
+    }
+};
+
+/**
+ * update the url so that the url can be shared to show same information
+ * https://stackoverflow.com/a/41542008/5416200
+ * @param {Object} response the data that updates the url {task: *task*}
+ */
+ function updateUrl(response) {
+    if ('URLSearchParams' in window) {
+        var searchParams = new URLSearchParams(window.location.search);
+        searchParams.set("task", response.task);
+        var newRelativePathQuery = window.location.pathname + '?' + searchParams.toString();
+        document.title = "ARC Task: " + response.task.toString();
+        window.history.pushState({"task": response.task, "pageTitle": document.title}, "", newRelativePathQuery);
+    }
 }
 
 function check() {
